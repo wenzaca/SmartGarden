bba10ba3b13f3256804f6dfe9d2750bc10cdb61f
Wendler Zacariotto <wenzaca@gmail.com>
2019-06-16 22:42:45 +0100

    Adding new method for reading the LDR data. Will be tested, if it works the code will be merged

--- sensor_readings.py
+++ sensor_readings.py
@@ -5,13 +5,17 @@ import datetime as datetime
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 import aws_publish_raspberry_script as core
+import RPi.GPIO as GPIO
 
 import log_util
 
-# Start SPI connection
+GPIO.setmode(GPIO.BOARD)
 
+# Pin setting
+moisture_pin = 0
+ldr_pin = 7
 
-# SPI configuration:
+# SPI configuration (moisture):
 SPI_PORT = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
@@ -30,11 +34,30 @@ def moisture_reading(channel):
     return round(100-(sum(data) / len(data)), 2)
 
 
-# Publish to the same topic in a loop forever
+# Read LDR data
+def light_reading(channel):
+    i = 0
+    data = []
+    while i < 100:
+        measure = 0.0  # Output on the pin for
+        GPIO.setup(channel, GPIO.OUT)
+        GPIO.output(channel, GPIO.LOW)
+        sleep(0.1)  # Change the pin back to input
+        GPIO.setup(channel, GPIO.IN)  # Count until the pin goes high
+        while GPIO.input(channel) == GPIO.LOW:
+            measure += 1
+        i += 1
+        data.append(measure)
+    return round((sum(data) / len(data)), 2)
+
+
+# Keep reading the data from the Sensors
 while True:
     try:
-        moisture = moisture_reading(0)
-        log_util.log_info(__name__, "Moisture reading: {}".format(moisture))
+        moisture = moisture_reading(moisture_pin)
+        light = light_reading(ldr_pin)
+        log_util.log_debug(__name__, "Moisture reading: {}".format(moisture))
+        log_util.log_debug(__name__, "Light reading: {}".format(light))
         message = {}
         message["id"] = 'id_smartgarden'
         now = datetime.datetime.now()
@@ -42,10 +65,12 @@ while True:
         message['Items'] = {"moisture1": moisture,
                             "temperature": 0,
                             "humidity": 0,
-                            "light": 0}
+                            "light": light}
 
         core.publish_readings(message)
         sleep(10)
     except KeyboardInterrupt as e:
         log_util.log_error(__name__, "Failed to read the data from the sensor due to {}".format(str(e)))
         break
+    finally:
+        GPIO.cleanup()
